11611818cb0eab8b3e915375670fea70596493ec
MRxwr <nasserhatab@gmail.com>
2023-05-19 22:40:37 +0300

    editing home screen and times

--- templates/main/selections.php
+++ templates/main/selections.php
@@ -1,12 +1,8 @@
 <div class="row m-0 w-100">
-	<div class="col-md-6">
+	<div class="col-md-12">
 		<label>Branch</label>
 		<select name="branch" class="form-control">
 			<option selected disabled value="0">Please select a Branch</option>
 		</select>
-	</div>
-	<div class="col-md-6">
-		<label>Date</label>
-		<input type="date" name="date" class="form-control">
 	</div>
 </div>
--- templates/main/times.php
+++ templates/main/times.php
@@ -1,18 +1,10 @@
 <div class="row m-0 w-100">
-	<div class="col-12">
-		<span>Time</span>
+	<div class="col-md-6">
+		<label>Date</label>
+		<input type="date" name="date" class="form-control">
 	</div>
-	<div class="col-12 p-3">
-		<div class="row m-0">
-		<?php
-		for( $i = 0; $i < 5; $i++){
-			?>
-			<div class="col d-flex align-items-center justify-content-center serviceBLk mx-2 mb-2 p-3">
-				<span><?php echo "Time{$i}"; ?></span>
-			</div>
-			<?php
-		}
-		?>
-		</div>
+	<div class="col-md-6">
+		<label>Time</label>
+		<input type="time" name="time" class="form-control">
 	</div>
 </div>

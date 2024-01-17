3cc9e9712c88c294f4328ac383cf6ee1ac836c7d
MRxwr <nasserhatab@gmail.com>
2024-01-17 11:45:36 +0300

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -6,8 +6,9 @@ $dbname = "u409066344_bookingDB";
 $dbPrefix = "cbt_";
 $dbconnect = new MySQLi($servername,$username,$password,$dbname);
 if ( $dbconnect->connect_error ){
-die("Connection Failed: " .$dbconnect->connect_error );
+    die("Connection Failed: " .$dbconnect->connect_error );
 }
 $sql = "SET CHARACTER SET utf8";
 $dbconnect->query($sql);
+die("success");
 ?>

6a986bee3b54f97935e1e087266782163784a0fc
MRxwr <nasserhatab@gmail.com>
2024-01-17 11:48:29 +0300

    Update logindb.php

--- admin/includes/logindb.php
+++ admin/includes/logindb.php
@@ -1,6 +1,6 @@
 <?php
 require_once("functions.php");
-die();
+die("succeess");
 if( $login = selectDB("employees","`email` LIKE '{$_POST["email"]}' AND `password` LIKE '".sha1($_POST["password"])."' AND `hidden` != '2' AND `status` = '0'") ){
 	$GenerateNewCC = md5(rand());
 	if( updateDB("employees",array("keepMeAlive"=>$GenerateNewCC),"`id` = '{$login[0]["id"]}'") ){

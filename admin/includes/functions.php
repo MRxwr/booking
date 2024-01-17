af2058b95f5e7b9f10e15fcd10f8432ea3dc52d4
MRxwr <nasserhatab@gmail.com>
2024-01-17 12:14:47 +0300

    Update functions.php

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -2,11 +2,11 @@
 require_once("config.php");
 require_once("functions/sql.php");
 require_once("functions/system.php");
+require_once("functions/general.php");
 
 /*
 require_once("functions/notification.php");
 require_once("functions/payment.php");
-require_once("functions/general.php");
 require_once("functions/cart.php");
 require_once("functions/svg.php");
 require_once("functions/vouchers.php");

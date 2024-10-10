cc2203f3722a3ebd0eb669b3e7d731279d67b49b
MRxwr <nasserhatab@gmail.com>
2024-10-11 02:04:43 +0300

    Update header.php

--- admin/template/header.php
+++ admin/template/header.php
@@ -1,5 +1,7 @@
 <?php
-require ("includes/functions.php");
+require_once("includes/config.php");
+require_once("includes/functions.php");
+require_once("includes/checksouthead.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">

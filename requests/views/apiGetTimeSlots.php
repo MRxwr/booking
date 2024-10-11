7f4cba54ce6b9d21068b49b69c4f41e7c68c9009
MRxwr <nasserhatab@gmail.com>
2024-10-12 02:09:00 +0300

    Update apiGetTimeSlots.php

--- requests/views/apiGetTimeSlots.php
+++ requests/views/apiGetTimeSlots.php
@@ -25,18 +25,42 @@ if( !isset($_POST["branchId"]) || empty($_POST["branchId"]) ){
         $start = substr($timeSlots[0]["startTime"],0,2);
         $close = substr($timeSlots[0]["closeTime"],0,2);
         $timeSlots = [];
-        $blockTime = selectDB("blocktime","`branchId` = '{$branchId}' AND `vendorId` = '{$vendorId}'");
-        if( $blockTime[0]["startDate"] <= $date && $blockTime[0]["endDate"] >= $date ){
-            $blockedStart = substr($blockTime[0]["fromTime"],0,2);
-            $blockedClose = substr($blockTime[0]["toTime"],0,2);
-            $blockedTimeArray = [];
-            for( $i = $blockedStart; $i < $blockedClose; $i++ ){
-                $blockedTimeArray[] = $blockedStart;
-                (int)$blockedStart++;
+        $blockedTimeVendor = [];
+        $blockedTimeBookings = [];
+
+        //vendor blocking time
+        if( $blockTime = selectDB("blocktime","`branchId` = '{$branchId}' AND `vendorId` = '{$vendorId}'") ){
+            if( $blockTime[0]["startDate"] <= $date && $blockTime[0]["endDate"] >= $date ){
+                $blockedStart = substr($blockTime[0]["fromTime"],0,2);
+                $blockedClose = substr($blockTime[0]["toTime"],0,2);
+                
+                for( $i = $blockedStart; $i < $blockedClose; $i++ ){
+                    $blockedTimeVendor[] = $blockedStart;
+                    (int)$blockedStart++;
+                }
+            }
+        }
+
+        //booking blocking number of seats per hour
+        if( $booking = selectDB("bookings","`branchId` = '{$branchId}' AND `vendorId` = '{$vendorId}' AND `serviceId` = '{$serviceId}' AND `bookedDate` = '{$date}'") ){
+            foreach( $booking as $book ){
+                $blockedTimeBookings[] = substr($book["bookedTime"],0,2);
+            }
+            if( $branches = selectDB("branches","`id` = '{$branchId}'") ){
+                $branchTotalSeats = $branches[0]["seats"];
+            }else{
+                $branchTotalSeats = '1';
+            }
+            //if timeslot repeated in booking = $branchTotalSeats then add it to blockedTimeBookings
+            for( $i = $start; $i < $close; $i++ ){
+                if( $branchTotalSeats == count(array_intersect($blockedTimeBookings,[$start])) ){
+                    $blockedTimeBookings[] = $start;
+                }
+                (int)$start++;
             }
         }
         for( $i = $start; $i < $close; $i++ ){
-            if( !in_array((int)$start, $blockedTimeArray) ){
+            if( !in_array((int)$start, $blockedTimeVendor) && !in_array((int)$start, $blockedTimeBookings) ){
                 $response["timeSlots"][] = ($start) . ":00 - " . ((int)($start)+1) . ":00";
             }
             (int)$start++;

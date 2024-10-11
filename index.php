b2ea0057958cebfb3f1fd38ef8b556b31086f19e
MRxwr <nasserhatab@gmail.com>
2024-10-11 05:13:59 +0300

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if( isset($_GET["vendorURL"]) && !empty($_GET["vendorURL"]) && $vendor = selectD
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Booking System</title>
+		<title>Booking System - <?php echo direction($vendor["enTitle"],$vendor["arTitle"]) ?></title>
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 		<script src="https://use.fontawesome.com/245c9398b0.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>

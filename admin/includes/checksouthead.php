bf24680d8011019a3d8a4fe154d2fcda132e04f1
MRxwr <nasserhatab@gmail.com>
2024-10-11 04:07:33 +0300

    Update checksouthead.php

--- admin/includes/checksouthead.php
+++ admin/includes/checksouthead.php
@@ -1,7 +1,6 @@
 <?php
 if ( isset($_COOKIE["createkuwaitAdmin"]) && !empty($_COOKIE["createkuwaitAdmin"]) ){
 	if ( $user = selectDBNew("employees",[$_COOKIE["createkuwaitAdmin"]],"`keepMeAlive` LIKE ? AND `status` = '0'","") ){
-		echo " 1 ";die();
 		$userID = $user[0]["id"];
 		$email = $user[0]["email"];
 		$username = $user[0]["fullName"];

038b738bdfd97a742b706959e9ca2d74474a6f65
MRxwr <nasserhatab@gmail.com>
2024-10-11 17:29:37 +0300

    Update index.php

--- requests/index.php
+++ requests/index.php
@@ -20,8 +20,36 @@ if ( isset(getallheaders()["Authorization"]) && !empty(getallheaders()["Authoriz
 	if( $checkToken = selectDBNew("tokens",[getallheaders()["Authorization"]],"`token` LIKE ?","") ){
 		$token = $checkToken[0];
 	}else{
-		$error = array("msg"=>"Invalid Token");
-		echo outputError($error);die();
+		if( isset($_SESSION["deviceId"]) && !empty($_SESSION["deviceId"]) ){
+			if( $deviceToken = selectDBNew("tokens",[$_SESSION["deviceId"]],"`deviceId` LIKE ?","") ){
+				jump2:
+				$newToken = password_hash(uniqid(), PASSWORD_BCRYPT);
+				if( $token = selectDBNew("tokens",[$newToken],"`token` LIKE ?","") ){
+					goto jump2;
+				}else{
+					updateDB("tokens",["token"=>$newToken],"`id` = '{$deviceToken[0]["id"]}'");
+					echo outputData(array("token"=>$newToken));die();
+				}
+			}else{
+				outputError(array("msg"=>"Invalid Device Id"));die();
+			}
+		}else{
+			jump:
+			$_SESSION["deviceId"] = md5(rand(00000,99999).time());
+			if( $deviceToken = selectDBNew("tokens",[$_SESSION["deviceId"]],"`deviceId` LIKE ?","") ){
+				goto jump;
+			}else{
+				insertDB("tokens",["deviceId"=>$_SESSION["deviceId"]]);
+				jump3:
+				$newToken = password_hash(uniqid(), PASSWORD_BCRYPT);
+				if( $token = selectDBNew("tokens",[$newToken],"`token` LIKE ?","") ){
+					goto jump3;
+				}else{
+					updateDB("tokens",["token"=>$newToken],"`deviceId` = '{$_SESSION["deviceId"]}'");
+					echo outputData(array("token"=>$newToken));die();
+				}
+			}
+		}
 	}
 }else{
 	$error = array("msg"=>"Please Set Authorization Token");

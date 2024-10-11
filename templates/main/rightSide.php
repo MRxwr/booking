500fc366bac58f529f5f3cdbe6edd54bc150dee1
MRxwr <nasserhatab@gmail.com>
2024-10-11 05:33:34 +0300

    save

--- templates/main/rightSide.php
+++ templates/main/rightSide.php
@@ -1,8 +1,8 @@
 <div class="col-8 p-0 d-none d-md-block" id="rightSide">
 	<div class="rightBg"></div>
 	<div class="bgOver"></div>
-	<div class="logoBg">
-	<img src="logos/<?php echo $vendor["logo"] ?>" class="logo">
+	<div class="logoBg" style="align-content: center;">
+		<img src="logos/<?php echo $vendor["logo"] ?>" class="logo">
 	</div>
 	<img src="https://i.imgur.com/BW29zDw.jpg" class="poweredByRight">
 </div>

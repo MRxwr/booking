123db13418ba40ab9b5becc5fe85dfbd70f625de
MRxwr <nasserhatab@gmail.com>
2024-10-12 14:20:48 +0300

    Update header.php

--- templates/main/header.php
+++ templates/main/header.php
@@ -3,8 +3,10 @@
 		<div class="col-2 d-flex align-items-center justify-content-center">
 			<?php echo direction("العربية","English") ?>
 		</div>
-		<div class="col-10 d-flex align-items-center justify-content-center">
+		<div class="col-8 d-flex align-items-center justify-content-center">
 			<?php echo direction($vendor["enTitle"],$vendor["arTitle"]) ?>
+		</div>
+		<div class="col-2 d-flex align-items-center justify-content-center">
 		</div>
 	</div>
 </div>

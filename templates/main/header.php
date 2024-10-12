631a7e9271caa74e32905ddaf5f294dd01f1642e
MRxwr <nasserhatab@gmail.com>
2024-10-12 15:28:02 +0300

    Update header.php

--- templates/main/header.php
+++ templates/main/header.php
@@ -1,7 +1,7 @@
 <div class="headerClass">
 	<div class="row m-0 w-100" style="height: 50px;">
 		<div class="col-2 d-flex align-items-center justify-content-center">
-			<a href="?lang=<?php echo direction("ar","en") ?>"><?php echo direction("العربية","English") ?></a>
+			<a href="?lang=<?php echo direction("ar","en") ?>" style="decoration:none"><?php echo direction("العربية","English") ?></a>
 		</div>
 		<div class="col-8 d-flex align-items-center justify-content-center">
 			<?php echo direction($vendor["enTitle"],$vendor["arTitle"]) ?>

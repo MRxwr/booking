07d7eb534470ec76c25e75004c486cdf84bb386e
MRxwr <nasserhatab@gmail.com>
2024-10-13 06:32:54 +0300

    Update apiGetTimeSlots.php

--- requests/views/apiGetTimeSlots.php
+++ requests/views/apiGetTimeSlots.php
@@ -23,7 +23,7 @@ if( !isset($_POST["branchId"]) || empty($_POST["branchId"]) ){
                     $ServiceTotalSeats = $services[0]["seats"];
                     $duration = $services[0]["period"];
                 }else{
-                    echo outputError("Service not exists for this branch");die();
+                    echo outputError("Service not exists for this vendor");die();
                 }
             }else{
                 echo outputError("Service not exists for this branch");die();

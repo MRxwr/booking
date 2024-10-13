8c2f48128bda8a4f788e98c5c217004ed3d46da7
MRxwr <nasserhatab@gmail.com>
2024-10-13 11:00:01 +0300

    Update sql.php

--- admin/includes/functions/sql.php
+++ admin/includes/functions/sql.php
@@ -175,7 +175,7 @@ function insertDB($table, $data){
     }
     $sql = rtrim($sql, ",");
     $placeholders = rtrim($placeholders, ",");
-    echo $sql .= ") VALUES ({$placeholders})";die();
+    $sql .= ") VALUES ({$placeholders})";
     $stmt = $dbconnect->prepare($sql);
     $types = str_repeat('s', count($data));
     $stmt->bind_param($types, ...array_values($data));

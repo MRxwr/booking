b6c03b8440065e1d4a85dd5b08dfaabb59d986d9
MRxwr <nasserhatab@gmail.com>
2024-10-14 02:54:26 +0300

    Update script.js

--- templates/main/script.js
+++ templates/main/script.js
@@ -21,33 +21,43 @@ $(window).ready(function() {
 		var name = $("#name").val();
 		var mobile = $("#mobile").val();
 		var email = $("#email").val();
+
+		// check cookie[createLang] if is set to ar then show alert else show alrt in english
+		if (getCookie("createLang") == "ar") {
+			var branchError = "يرجى تحديد الفرع";
+			var vendorError = "يرجى تحديد البائع";
+			var serviceError = "يرجى تحديد الخدمة";
+			var dateError = "يرجى تحديد التاريخ";
+			var timeError = "يرجى تحديد الوقت";
+			var nameError = "يرجى ادخال الاسم";
+			var mobileError = "يرجى ادخال رقم الجوال";
+			var emailError = "يرجى ادخال البريد الالكتروني";
+			var termsError = "يرجى الموافقة على الشروط والاحكام";
+			var paymentError = "حدث خطأ ، يرجى المحاولة مرة اخرى";
+			var serverError = "لا يمكن الاتصال بخادم البيانات للدفع ، يرجى المحاولة مرة اخرى";
+		}else{
+			var branchError = "Please select a branch";
+			var vendorError = "Please select a vendor";
+			var serviceError = "Please select a service";
+			var dateError = "Please select a date";
+			var timeError = "Please select atime";
+			var nameError = "Please enter your name";
+			var mobileError = "Please enter your mobile with country code";
+			var emailError = "Please enter your email";
+			var termsError = "Please agree to terms and conditions";
+			var paymentError = "Something went wrong. Please try again.";
+			var serverError = "could not make server connection to payment gateway, please try again.";
+		}
 		// check all above values if they are = to 0 or not and show seperate alert for each one
-		if (branchId == 0 ){
-			alert("Please select a branch");
-			return false;
-		}else if(vendorId == 0){
-			alert("Please select a vendor");
-			return false;
-		}else if( serviceId == 0){
-			alert("Please select a service");
-			return false;
-		}else if(date == 0 ){
-			alert("Please select a date");
-			return false;
-		}else if ( time == 0 ) {
-			alert("Please select atime");
-			return false;
-		}else if( name == "" ){
-			alert("Please enter your name");
-		}else if ( mobile == "" ){
-			alert("Please enter your mobile with country code")
-			return false;
-		}else if ( email == "" ){
-			alert("Please enter your email")
-			return false;
-		}else if ( $("input[type=checkbox]").is(":checked") == false ){
-			alert("Please agree to terms and conditions")
-			return false;
+		if 		( branchId == 0 ){	alert(branchError);	return false;
+		}else if( vendorId == 0	){	alert(vendorError);	return false;
+		}else if( serviceId == 0){	alert(serviceError);return false;
+		}else if( date == 0 	){	alert(dateError);	return false;
+		}else if( time == 0 	){	alert(timeError);	return false;
+		}else if( name == "" 	){	alert(nameError);	return false;
+		}else if( mobile == "" 	){	alert(mobileError);	return false;
+		}else if( email == "" 	){	alert(emailError);	return false;
+		}else if( $("input[type=checkbox]").is(":checked") == false ){alert(termsError);return false;
 		}
 		$.ajax({
 			type: "POST",
@@ -65,17 +75,16 @@ $(window).ready(function() {
 			  }
 			}
 		  }).done(function(result){
-			console.log(result);
 			  if( result.ok === true ){
 				window.location.href = result.data.data.data.link;
 			  }else{
 				// show error message
-				alert('Something went wrong. Please try again.');
+				alert(paymentError);
 				return false;
 			  }
 		  }).fail(function(){
 			// show error message
-			alert('could not make server connection to payment gateway, please try again.');
+			alert(serverError);
 			return false;
 		  });
 	});

d30b5b57538e7636c4e1671424c6590a3ed0197f
MRxwr <nasserhatab@gmail.com>
2024-10-14 14:01:04 +0300

    save

--- templates/main/info.php
+++ templates/main/info.php
@@ -38,3 +38,19 @@
 	</div>
 	</div>
 </form>
+
+<div class="modal fade" id="terms" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal-dialog" role="document">
+	<div class="modal-content">
+	<div class="modal-header">
+		<h5 class="modal-title" id="exampleModalLabel"><?php echo direction("Terms and Conditions","الشروط والاحكام") ?></h5>
+		<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+		<span aria-hidden="true">&times;</span>
+		</button>
+	</div>
+	<div class="modal-body" id="termsModalBody">
+		<?php echo direction($vendor["enTerms"],$vendor["arTerms"]) ?>
+	</div>
+	</div>
+</div>
+</div>

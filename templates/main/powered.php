c83324edda01f66adb6e5dde22930bd4c6320765
MRxwr <nasserhatab@gmail.com>
2024-10-14 19:25:06 +0300

    Update powered.php

--- templates/main/powered.php
+++ templates/main/powered.php
@@ -2,10 +2,10 @@
 	<div class="col-12 p-0 d-block d-sm-none">
 		<div class="row m-0 w-100">
 			<div class="col-12 text-center">
-				
+				<img src="https://i.imgur.com/rXGZxOF.png" class="poweredMobile">
 			</div>
 			<div class="col-12 text-center">
-			<a href="https://createkuwait.com" style="text-decoration: none;" target="_blank"><label>2018 <img src="https://i.imgur.com/rXGZxOF.png" class="poweredMobile" > createkuwait</label></a>
+				<a href="https://createkuwait.com" style="text-decoration: none;" target="_blank"><label>2018 (c) createkuwait</label></a>
 			</div>
 		</div>		
 	</div>

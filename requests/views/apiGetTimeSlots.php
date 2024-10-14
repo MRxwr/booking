79bae79a2a67de5b6fb74b91f4b1cce98696dda3
MRxwr <nasserhatab@gmail.com>
2024-10-14 13:44:57 +0300

    Update apiGetTimeSlots.php

--- requests/views/apiGetTimeSlots.php
+++ requests/views/apiGetTimeSlots.php
@@ -50,7 +50,6 @@ if( !isset($_POST["branchId"]) || empty($_POST["branchId"]) ){
                     (int)$blockedStart++;
                 }
             }
-            var_dump($blockedTimeVendor);
         }
 
         //booking blocking number of seats per hour
@@ -84,6 +83,7 @@ if( !isset($_POST["branchId"]) || empty($_POST["branchId"]) ){
         // removeing all blocked time from timeSlots
         $startTime = ($start) . ":00";
         while( true ){
+            var_dump(!in_array((int)$start, $blockedTimeVendor) . " {$start}");
             if( !in_array((int)$start, $blockedTimeVendor) && !in_array((int)$start, $blockedTimeBookings) ){
                  //$response["timeSlots"][] = ($start) . ":00 - " . ((int)($start)+1) . ":00";
                  $endTime = date('H:i', strtotime('+'.$duration.' minutes', strtotime($startTime)));

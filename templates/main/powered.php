2f302a9eb45161cab845cb6bbf00d33fa4d391c7
MRxwr <nasserhatab@gmail.com>
2024-10-14 19:43:53 +0300

    Update powered.php

--- templates/main/powered.php
+++ templates/main/powered.php
@@ -6,7 +6,7 @@
 				<img src="https://i.imgur.com/BW29zDw.jpg" class="poweredMobile" >
 			</div>
 			<div class="col-12 text-center">
-				<label>2018 (c) createKuwait</label>
+				<label>2018 (c) createkuwait</label>
 			</div>
 		</div>		
 	</div>

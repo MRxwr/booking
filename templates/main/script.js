6262541cc8cfe8725737a5cc268039482a1ab2ec
MRxwr <nasserhatab@gmail.com>
2024-10-14 03:09:01 +0300

    Update script.js

--- templates/main/script.js
+++ templates/main/script.js
@@ -100,3 +100,9 @@ $(window).resize(function() {
 	}
 });
 
+function getCookie(name) {
+	const value = `; ${document.cookie}`;
+	const parts = value.split(`; ${name}=`);
+	if (parts.length === 2) return parts.pop().split(';').shift();
+  }
+

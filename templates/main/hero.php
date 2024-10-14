62fe89832e0f746cac7233e4c1afb1a5d2b4d391
MRxwr <nasserhatab@gmail.com>
2024-10-14 03:39:02 +0300

    Update hero.php

--- templates/main/hero.php
+++ templates/main/hero.php
@@ -1,7 +1,7 @@
 <div class="row m-0 w-100">
 	<div class="col-12 p-0 d-block d-md-none">
 		<div class="heroBg">
-			<div class="heroLogoBg" style="align-content: center;<?php echo direction("bottom: -50px;left: 30%;","bottom: -50px;left: -30%;") ?>">
+			<div class="heroLogoBg" style="align-content: center;<?php echo direction("bottom: -50px;left: 32%;","bottom: -50px;left: -32%;") ?>">
 				<img src="<?php echo "logos/{$vendor["logo"]}" ?>" class="heroLogo">
 			</div>
 		</div>

ec70cfca56d238d470f01eedf1bc36e9c25c9617
MRxwr <nasserhatab@gmail.com>
2024-10-14 03:15:20 +0300

    Update selections.php

--- templates/main/selections.php
+++ templates/main/selections.php
@@ -147,6 +147,12 @@
   $(document).on("click",".serviceBLk", function(){
 	var serviceId = $(this).attr("id");
 	$("input[name='serviceId']").val(serviceId);
+	$("input[name='selectedTime']").val(0);
+	$("input[name='selectedDate']").val(0);
+	//reset and remove all option from select name time
+	$("#time-select").html('<option selected disabled value="0><?php echo direction("Please select a Time","الرجاء تحديد الوقت") ?></option>');
+	//reset and remove all option from input name date
+	$("input[name='date']").val(0);
 	// give it style active and remove the active from all other services 
 	$(".serviceBLk").removeClass("activeService");
 	$(this).addClass("activeService");

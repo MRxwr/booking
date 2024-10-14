0dac6eacf63d054e0d1066ebd0a6493a91284407
MRxwr <nasserhatab@gmail.com>
2024-10-14 19:25:48 +0300

    save

--- templates/main/powered.php
+++ templates/main/powered.php
@@ -5,7 +5,7 @@
 				<img src="https://i.imgur.com/rXGZxOF.png" class="poweredMobile">
 			</div>
 			<div class="col-12 text-center">
-				<a href="https://createkuwait.com" style="text-decoration: none;" target="_blank"><label>2018 (c) createkuwait</label></a>
+				<a href="https://createkuwait.com" style="text-decoration: none;color: #4e1c07;" target="_blank"><label>2018 (c) createkuwait</label></a>
 			</div>
 		</div>		
 	</div>

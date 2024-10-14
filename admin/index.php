3793f1365a697c49d93242c6f56d35203ee90ba1
MRxwr <nasserhatab@gmail.com>
2024-10-14 12:05:31 +0300

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -15,6 +15,7 @@ if ( isset($_GET["hide"]) || isset($_GET["show"]) || isset($_GET["delId"]) || is
 		unset($_POST["update"]);
 		if ( isset($_POST["sm"]) && !empty($_POST["sm"]) ){
 			$_POST["socialMedia"] = json_encode($_POST["sm"]);
+			unset($_POST["sm"]);
 		}
 		if( isset($_FILES['logo']) && is_uploaded_file($_FILES['logo']['tmp_name']) ){
 			$_POST["logo"] = uploadImage($_FILES["logo"]["tmp_name"]);

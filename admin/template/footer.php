55e49bf833ee5cfd92a7d65f093bd12b753b8ce4
MRxwr <nasserhatab@gmail.com>
2024-10-31 11:36:06 +0300

    Update footer.php

--- admin/template/footer.php
+++ admin/template/footer.php
@@ -50,52 +50,5 @@
 		
 	<!-- Init JavaScript -->
 	<script src="dist/js/init.js"></script>
-
-<!-- Initialize FullCalendar after dependencies are loaded -->
-<script>
-    $(document).ready(function() {
-        $('#calendar').fullCalendar({
-            header: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'month,agendaWeek,agendaDay'
-            },
-            editable: true,
-            events: [
-                {
-                    title: 'Booking 1',
-                    start: '2024-11-05T10:00:00',
-                    end: '2024-11-05T12:00:00',
-                    description: 'Customer Name: John Doe'
-                },
-                {
-                    title: 'Booking 2',
-                    start: '2024-11-06T14:00:00',
-                    end: '2024-11-06T15:00:00',
-                    description: 'Customer Name: Jane Doe'
-                }
-                // Additional bookings
-            ],
-            eventRender: function(event, element) {
-                if (event.description) {
-                    element.find('.fc-title').append('<br/><span class="fc-description">' + event.description + '</span>');
-                }
-            }
-        });
-    });
-	$(document).ready(function() {
-    $('#calendar').fullCalendar({
-        dayClick: function(date, jsEvent, view) {
-            if (typeof tooltip !== 'undefined') {
-                tooltip.show(); // Adjust based on your tooltip implementation
-            } else {
-                console.log('Tooltip library is not loaded.');
-            }
-        }
-    });
-});
-
-</script>
-
 </body>
 </html>

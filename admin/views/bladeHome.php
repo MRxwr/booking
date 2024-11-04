f471ed6e85c6b7c34a39b92e123d1876042a66d1
Sonjoy Bhadra <sbhadra0@gmail.com>
2024-11-04 17:21:26 +0530

    Update bladeHome.php

--- admin/views/bladeHome.php
+++ admin/views/bladeHome.php
@@ -2,4 +2,22 @@
 	<div class="col-12 p-3 text-center" style="font-weight:700; font-size:20px">
 	<?php echo direction("Welcome To Create Booking Vendor Control Panel","أهلاً بك في لوحة التحكم الخاصة بالعملاء"); ?>
 	</div>
+	<div class="col-lg-12">
+		<div class="panel panel-default card-view">
+			<div class="panel-wrapper collapse in">
+				<div class="panel-body">
+					<div class="add-event-wrap">
+						<div class="calendar-event btn btn-success">My Event One <a href="javascript:void(0);" class="remove-calendar-event"><i class="fa fa-times fa-fw"></i></a></div>
+						<div class="calendar-event btn btn-info">My Event Two <a href="javascript:void(0);" class="remove-calendar-event"><i class="fa fa-times fa-fw"></i></a></div>
+						<div class="calendar-event btn btn-warning">My Event Three <a href="javascript:void(0);" class="remove-calendar-event"><i class="fa fa-times fa-fw"></i></a></div>
+						<div class="calendar-event btn btn-primary">My Event Four <a href="javascript:void(0);" class="remove-calendar-event"><i class="fa fa-times fa-fw"></i></a></div>
+						<input type="text" placeholder="Add Event and hit enter" class="form-control add-event mt-30">
+					</div>
+					<div class="calendar-wrap mt-40">
+						<div id="calendar"></div>
+					</div>
+				</div>
+			</div>
+		</div>	
+	</div>	
 </div>

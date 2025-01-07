56a9a4d38cf8275b58423e10119608daa1980c40
MRxwr <nasserhatab@gmail.com>
2025-01-07 13:20:07 +0300

    Update selections.php

--- templates/main/selections.php
+++ templates/main/selections.php
@@ -215,7 +215,7 @@
 			return false;
 		}
 		var timeSlots = data.data.timeSlots;
-		alert(timeSlots);
+		console.log(timeSlots);
 		var timeSlotHTML = "";
 		timeSelect.innerHTML = '<option value="0">Please select a time</option>';
 		var currentTime = new Date(); // Get the current device time

075239f675f08ce9bab5f7460a3ceaa33969cb0b
MRxwr <nasserhatab@gmail.com>
2025-01-07 12:54:39 +0300

    Update bladeBranches.php

--- admin/views/bladeBranches.php
+++ admin/views/bladeBranches.php
@@ -193,7 +193,7 @@ if( isset($_GET["updateTimes"]) || isset($_GET["updateServices"]) ){
 	$(document).on("click",".serviceBtn", function(){
 		var branchId = $(this).attr("id");
 		$.ajax({
-			url: "/admin/requests/index.php?r=services",
+			url: "/admin/requests.php?r=services",
 			type: "POST",
 			data: { id : branchId },
 			success: function(response) {

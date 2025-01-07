a51c8147810edd1bd7dd26c8b63c5eebb10630c0
MRxwr <nasserhatab@gmail.com>
2025-01-07 10:59:33 +0300

    Update leftSideBar.php

--- admin/template/leftSideBar.php
+++ admin/template/leftSideBar.php
@@ -8,8 +8,9 @@
 if( $pages = selectDB("pages","`status` = '0' AND `hidden` = '0' AND `section` = '0' ORDER BY `order` ASC") ){
 	$listOfAllowedPages = "";
 	if( $roles = selectDB("roles","`id` = '{$userType}'") ){
-		var_dump($roles[0]["pages"]);
+		
 		$list = json_decode($roles[0]["pages"],true);
+		var_dump($list);
 		$listOfAllowedPages = implode(",", $list);
 		if( selectDB("pages","`enTitle` LIKE '{$_GET["v"]}' AND `id` IN ({$listOfAllowedPages})") ){
 		}else{

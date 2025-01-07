f1e248b80926f54d41f3cbcc306cf29ba3c50601
MRxwr <nasserhatab@gmail.com>
2025-01-08 00:48:48 +0300

    Update apiGetTimeSlots.php

--- requests/views/apiGetTimeSlots.php
+++ requests/views/apiGetTimeSlots.php
@@ -68,7 +68,6 @@ if( !isset($_POST["branchId"]) || empty($_POST["branchId"]) ){
 
         //booking blocking number of seats per hour
         if( $booking = selectDBNew("bookings",[$branchId,$vendorId,$date],"`branchId` = ? AND `vendorId` = ? AND `bookedDate` = ? AND (`status` = '1' OR (`status` = '0' AND TIMESTAMPDIFF(MINUTE, `date`, NOW()) < 15))","") ){
-            var_dump($booking);
             foreach( $booking as $book ){
                 $bookedTimes[] = substr($book["bookedTime"],0,2);
             }
@@ -83,7 +82,6 @@ if( !isset($_POST["branchId"]) || empty($_POST["branchId"]) ){
 
         //booking blocking of services number of seats per hour
         if( $booking = selectDB("bookings","`branchId` = '{$branchId}' AND `vendorId` = '{$vendorId}' AND `serviceId` = '{$serviceId}' AND `bookedDate` = '{$date}' AND (`status` = '1' OR (`status` = '0' AND TIMESTAMPDIFF(MINUTE, `date`, NOW()) < 15))") ){
-            
             foreach( $booking as $book ){
                 $bookedService[] = substr($book["bookedTime"],0,2);
             }

47603f2fb869b9a870a9dbf396d68cda8efb6be4
MRxwr <nasserhatab@gmail.com>
2025-01-07 12:55:48 +0300

    Update request.php

--- admin/request.php
+++ admin/request.php
@@ -2,7 +2,6 @@
 require ("includes/config.php");
 require ("includes/functions.php");
 require ("includes/checksouthead.php");
-var_dump($_GET["r"]);die();
 if( isset($_GET["r"]) && searchFile("requests","{$_GET["r"]}.php") ){
 	require_once("requests/".searchFile("requests","{$_GET["r"]}.php"));
 }else{

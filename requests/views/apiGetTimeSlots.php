4d4fc8e006146f6a80fe8c970d8690388c56d849
MRxwr <nasserhatab@gmail.com>
2025-01-08 00:28:00 +0300

    Update apiGetTimeSlots.php

--- requests/views/apiGetTimeSlots.php
+++ requests/views/apiGetTimeSlots.php
@@ -1,143 +1,119 @@
 <?php
-if (!isset($_POST["branchId"]) || empty($_POST["branchId"])) {
+if( !isset($_POST["branchId"]) || empty($_POST["branchId"]) ){
     echo outputError("branch is required");
-} elseif (!isset($_POST["date"]) || empty($_POST["date"])) {
+}elseif( !isset($_POST["date"]) || empty($_POST["date"]) ){
     echo outputError("date is required");
-} elseif (!isset($_POST["vendorId"]) || empty($_POST["vendorId"])) {
+}elseif( !isset($_POST["vendorId"]) || empty($_POST["vendorId"]) ){
     echo outputError("vendor is required");
-} elseif (!isset($_POST["serviceId"]) || empty($_POST["serviceId"])) {
+}elseif( !isset($_POST["serviceId"]) || empty($_POST["serviceId"]) ){
     echo outputError("service is required");
-} else {
+}else{
     $branchId = $_POST["branchId"];
     $serviceId = $_POST["serviceId"];
     $vendorId = $_POST["vendorId"];
     $date = $_POST["date"];
     $day = date('w', strtotime($date));
-
-    $timeSlotsData = selectDBNew(
-        "times",
-        [$branchId, $day, $vendorId],
-        "`branchId` = ? AND `day` = ? AND `vendorId` = ? AND `status` = '0' AND `hidden` = '0' ORDER BY `id` DESC",
-        ""
-    );
-    if (!$timeSlotsData) {
-        $response["timeSlots"][0] = "No time slots available";
-        echo outputError($response);
-        die();
-    }
-
-    $openTime  = $timeSlotsData[0]["startTime"];
-    $closeTime = $timeSlotsData[0]["closeTime"];
-    $startHour = (int) substr($openTime, 0, 2);
-    $closeHour = (int) substr($closeTime, 0, 2);
-
-    $branches = selectDBNew(
-        "branches",
-        [$branchId, $vendorId],
-        "`id` = ? AND `status` = '0' AND `hidden` = '0' AND `vendorId` = ?",
-        ""
-    );
-    if (!$branches) {
-        echo outputError("Branch not exists for this vendor");
-        die();
-    }
-    $branchTotalSeats = $branches[0]["seats"];
-    if (!in_array($serviceId, json_decode($branches[0]["services"], true))) {
-        echo outputError("Service not exists for this branch");
-        die();
-    }
-
-    $services = selectDBNew(
-        "services",
-        [$serviceId],
-        "`id` = ? AND `status` = '0' AND `hidden` = '0'",
-        ""
-    );
-    if (!$services) {
-        echo outputError("Service has been removed by vendor");
-        die();
-    }
-    $ServiceTotalSeats = $services[0]["seats"];
-    $duration = (int) $services[0]["period"];
-
-    $bookingsBranch = selectDBNew(
-        "bookings",
-        [$branchId, $vendorId, $date],
-        "`branchId` = ? AND `vendorId` = ? AND `bookedDate` = ? 
-         AND (
-             `status` = '1'
-             OR (`status` = '0' AND TIMESTAMPDIFF(MINUTE, `date`, NOW()) < 15)
-         )",
-        ""
-    );
-
-    $startTimestamp = strtotime($startHour . ":00", strtotime($date));
-    $closeTimestamp = strtotime($closeHour . ":00", strtotime($date));
-    $durationSeconds = $duration * 60;
-    $response = ["timeSlots" => []];
-
-    while ($startTimestamp < $closeTimestamp) {
-        $endTimestamp = $startTimestamp + $durationSeconds;
-        if ($endTimestamp > $closeTimestamp) {
-            break;
+    if( $timeSlots = selectDBNew("times",[$branchId,$day,$vendorId],"`branchId` = ? AND `day` = ? AND `vendorId` = ? AND `status` = '0' AND `hidden` = '0' ORDER BY `id` DESC","") ){
+        //Get Branch Details
+        if( $branches = selectDBNew("branches",[$branchId,$vendorId],"`id` = ? AND `status` = '0' AND `hidden` = '0' AND `vendorId` = ?","") ){
+            $branchTotalSeats = $branches[0]["seats"];
+            // get services for branch
+            if ( in_array($serviceId,json_decode($branches[0]["services"],true)) ){
+                if( $services = selectDBNew("services",[$serviceId],"`id` = ? AND `status` = '0' AND `hidden` = '0'","") ){
+                    $ServiceTotalSeats = $services[0]["seats"];
+                    $duration = $services[0]["period"];
+                }else{
+                    echo outputError("Service has been removed by vendor");die();
+                }
+            }else{
+                echo outputError("Service not exists for this branch");die();
+            }
+        }else{
+            echo outputError("Branch not exists for this vendor");die();
         }
-        if (!isSlotBlocked(
-            $startTimestamp,
-            $endTimestamp,
-            $bookingsBranch,
-            $branchTotalSeats,
-            $ServiceTotalSeats,
-            $duration,
-            $date
-        )) {
-            $slotStart = date('H:i', $startTimestamp);
-            $slotEnd   = date('H:i', $endTimestamp);
-            $response["timeSlots"][] = $slotStart . " - " . $slotEnd;
-        }
-        $startTimestamp = $endTimestamp;
-    }
-
-    if (empty($response["timeSlots"])) {
-        $response["timeSlots"][0] = "No time slots available";
-        echo outputError($response);
-        die();
-    }
-
-    echo outputData($response);
-    die();
-}
-
-function isSlotBlocked(
-    $slotStart,
-    $slotEnd,
-    $bookings,
-    $branchSeats,
-    $serviceSeats,
-    $serviceDuration,
-    $bookingDate
-) {
-    $branchUsage  = 0;
-    $serviceUsage = 0;
-    foreach ($bookings as $book) {
-        $bookedStart = strtotime($bookingDate . " " . $book["bookedTime"]);
-        $bookedEnd   = $bookedStart + ($serviceDuration * 60);
-        if (timeRangesOverlap($slotStart, $slotEnd, $bookedStart, $bookedEnd)) {
-            $branchUsage++;
-            if ((int) $book["serviceId"] === (int) $_POST["serviceId"]) {
-                $serviceUsage++;
+        $start = substr($timeSlots[0]["startTime"],0,2);
+        $close = substr($timeSlots[0]["closeTime"],0,2);
+        $timeSlots = [];
+        $blockedTimeVendor = [];
+        $blockedTimeBookings = [];
+        $bookedTimes = [];
+        $bookedService = [];
+        $bookedTimeService = [];
+        //vendor blocking time
+        if( $blockTime = selectDB("blocktime","`branchId` = '{$branchId}' AND `vendorId` = '{$vendorId}' AND `serviceId` = '0' AND `hidden` = '0' AND `status` = '0'  ORDER BY `id` DESC LIMIT 1") ){
+            if( $blockTime[0]["startDate"] <= $date && $blockTime[0]["endDate"] >= $date ){
+                $blockedStart = substr($blockTime[0]["fromTime"],0,2);
+                $blockedClose = substr($blockTime[0]["toTime"],0,2);
+                
+                for( $i = $blockedStart; $i < $blockedClose; $i++ ){
+                    $blockedTimeVendor[] = (string)$blockedStart;
+                    (int)$blockedStart++;
+                }
             }
         }
-    }
-    if ($branchUsage >= $branchSeats) {
-        return true;
-    }
-    if ($serviceUsage >= $serviceSeats) {
-        return true;
-    }
-    return false;
-}
 
-function timeRangesOverlap($startA, $endA, $startB, $endB) {
-    return ($startA < $endB && $endA > $startB);
+        //vendor blocking time
+        if( $blockTime = selectDB("blocktime","`branchId` = '{$branchId}' AND `vendorId` = '{$vendorId}' AND `serviceId` = '{$serviceId}' AND `hidden` = '0' AND `status` = '0' ORDER BY `id` DESC LIMIT 1") ){
+            if( $blockTime[0]["startDate"] <= $date && $blockTime[0]["endDate"] >= $date ){
+                $blockedStart = substr($blockTime[0]["fromTime"],0,2);
+                $blockedClose = substr($blockTime[0]["toTime"],0,2);
+                
+                for( $i = $blockedStart; $i < $blockedClose; $i++ ){
+                    $blockedTimeVendor[] = (string)$blockedStart;
+                    (int)$blockedStart++;
+                }
+            }
+        }
+
+        //booking blocking number of seats per hour
+        if( $booking = selectDBNew("bookings",[$branchId,$vendorId,$date],"`branchId` = ? AND `vendorId` = ? AND `bookedDate` = ? AND (`status` = '1' OR (`status` = '0' AND TIMESTAMPDIFF(MINUTE, `date`, NOW()) < 15))","") ){
+            foreach( $booking as $book ){
+                $bookedTimes[] = substr($book["bookedTime"],0,2);
+            }
+            $counter = (int)($start);
+            for( $i = $start; $i < $close; $i++ ){
+                if( $branchTotalSeats == count(array_intersect($bookedTimes,[$counter])) ){
+                    $blockedTimeBookings[] = $counter;
+                }
+                $counter++;
+            }
+        }
+
+        //booking blocking of services number of seats per hour
+        if( $booking = selectDB("bookings","`branchId` = '{$branchId}' AND `vendorId` = '{$vendorId}' AND `serviceId` = '{$serviceId}' AND `bookedDate` = '{$date}' AND (`status` = '1' OR (`status` = '0' AND TIMESTAMPDIFF(MINUTE, `date`, NOW()) < 15))") ){
+            foreach( $booking as $book ){
+                $bookedService[] = substr($book["bookedTime"],0,2);
+            }
+            $counter = (int)($start);
+            for( $i = $start; $i < $close; $i++ ){
+                if( $ServiceTotalSeats == count(array_intersect($bookedService,[$counter])) ){
+                    $blockedTimeBookings[] = $counter;
+                }
+                $counter++;
+            }
+        }
+
+        $startTimestamp = strtotime($start . ":00"); 
+        $closeTimestamp = strtotime($close . ":00"); 
+        $durationInSeconds = $duration * 60; // convert minutes to seconds
+        while ($startTimestamp < $closeTimestamp) {
+            $endTimestamp = $startTimestamp + $durationInSeconds;
+            if ($endTimestamp > $closeTimestamp) {
+                break;
+            }
+            $currentSlotStart = date('H:i', $startTimestamp);
+            $currentSlotEnd   = date('H:i', $endTimestamp);
+            $startHour = (int) date('H', $startTimestamp);
+            if (!in_array($startHour, $blockedTimeVendor) && !in_array($startHour, $blockedTimeBookings)) {
+                $response["timeSlots"][] = $currentSlotStart . " - " . $currentSlotEnd;
+            }
+            $startTimestamp = $endTimestamp;
+        }
+        echo outputData($response);die();
+    }else{
+        $response["timeSlots"][0] = "No time slots available";
+        echo outputError($response);die();
+    }
 }
+//
 ?>

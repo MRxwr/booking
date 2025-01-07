3df200ea56e6e9bea2626dffebcd77412066ba07
MRxwr <nasserhatab@gmail.com>
2025-01-08 00:38:23 +0300

    Update apiGetTimeSlots.php

--- requests/views/apiGetTimeSlots.php
+++ requests/views/apiGetTimeSlots.php
@@ -54,10 +54,9 @@ if( !isset($_POST["branchId"]) || empty($_POST["branchId"]) ){
 
         //vendor blocking time
         if( $blockTime = selectDB("blocktime","`branchId` = '{$branchId}' AND `vendorId` = '{$vendorId}' AND `serviceId` = '{$serviceId}' AND `hidden` = '0' AND `status` = '0' ORDER BY `id` DESC LIMIT 1") ){
-            var_dump($blockTime);die();
             if( $blockTime[0]["startDate"] <= $date && $blockTime[0]["endDate"] >= $date ){
-                $blockedStart = substr($blockTime[0]["fromTime"],0,2);
-                $blockedClose = substr($blockTime[0]["toTime"],0,2);
+                echo $blockedStart = substr($blockTime[0]["fromTime"],0,2);
+                echo $blockedClose = substr($blockTime[0]["toTime"],0,2);
                 
                 for( $i = $blockedStart; $i < $blockedClose; $i++ ){
                     $blockedTimeVendor[] = (string)$blockedStart;

75b79dc38ece730fc400451c5dd09f407843dc8a
MRxwr <nasserhatab@gmail.com>
2025-01-07 13:48:20 +0300

    Update apiGetTimeSlots.php

--- requests/views/apiGetTimeSlots.php
+++ requests/views/apiGetTimeSlots.php
@@ -104,7 +104,7 @@ if( !isset($_POST["branchId"]) || empty($_POST["branchId"]) ){
                  }else{
                     $response["timeSlots"][] = $startTime . " - " . $endTime;
                  }
-                 echo $start++; echo " ";
+                 echo $start = str_pad($start+1, 2, "0", STR_PAD_LEFT); echo " ";
                  $startTime = $endTime;
             }else{
                 $start++;

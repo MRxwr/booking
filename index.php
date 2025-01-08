e78a184179bac49a4d801bd42bfb85081ad4562c
MRxwr <nasserhatab@gmail.com>
2025-01-08 14:07:25 +0300

    Update index.php

--- index.php
+++ index.php
@@ -39,6 +39,8 @@ if( isset($_REQUEST["vendorURL"]) && !empty($_REQUEST["vendorURL"]) && $vendor =
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title><?php echo direction($vendor["enTitle"],$vendor["arTitle"]) ?> - Booking System</title>
+		<!-- add fav icon -->
+		<link rel="icon" type="image/png" href="logos/<?php echo $vendor["logo"] ?>">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 		<script src="https://use.fontawesome.com/245c9398b0.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>

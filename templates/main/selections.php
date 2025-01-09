39aae637aebdd766a44a281abf9dcb639fe50b4c
MRxwr <nasserhatab@gmail.com>
2025-01-09 13:12:46 +0300

    Update selections.php

--- templates/main/selections.php
+++ templates/main/selections.php
@@ -257,7 +257,7 @@ if( $vendor["type"] == "3" ){
 	// Fill typesBlk with radio inputs based on filtered pictureTypes
 	$(".typesBLK").empty();
 	$.each(filteredPictureTypes, function(key, value) {
-		$types = "<div class='col-8 pb-1'><input type='radio' name='pictureType' value='"+value.id+"'><label>"+value.title+"</label></div><div class='col-4'>"+value.price+"/KD</div>";
+		$types = "<div class='col-10 pb-1'><input type='radio' name='pictureType' value='"+value.id+"'><label>"+value.title+"</label></div><div class='col-2'>"+value.price+"-/KD</div>";
 		$(".typesBLK").append($types);
 	});
 

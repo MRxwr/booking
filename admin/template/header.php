fd739aaea3836f9f9ddd1517020a95cdba16f6e0
MRxwr <nasserhatab@gmail.com>
2025-01-09 03:20:45 +0300

    Update header.php

--- admin/template/header.php
+++ admin/template/header.php
@@ -1,4 +1,8 @@
 <?php
+//show all php errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 require_once("includes/config.php");
 require_once("includes/functions.php");
 require_once("includes/checksouthead.php");

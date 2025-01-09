8e0dd1d39e8c003669f944661d993bfdbba16fcb
MRxwr <nasserhatab@gmail.com>
2025-01-09 11:45:15 +0300

    Update selections.php

--- templates/main/selections.php
+++ templates/main/selections.php
@@ -62,7 +62,7 @@
   // Store pictureTypes data in a JavaScript object
   var pictureTypes = [
 	<?php
-	$pictureTypes = selectDB("picturetypes","`status` = '0' AND `hidden` = '0' AND `vendorId` = '{$vendor["id"]}' ORDER BY `id` ASC");
+	$pictureTypes = selectDB("picturetype","`status` = '0' AND `hidden` = '0' AND `vendorId` = '{$vendor["id"]}' ORDER BY `id` ASC");
 	foreach($pictureTypes as $pictureType){
 		$service = selectDB("services","`listTypes` = '%{$pictureType["serviceId"]}%' AND `status` = '0' AND `hidden` = '0' AND `vendorId` = '{$vendor["id"]}' ORDER BY `id` ASC");
 	  echo "{ id: '".$pictureType["id"]."', serviceId: '".$service[0]["id"]."' ,price: '".$pictureType["price"]."', title: '".direction($pictureType["enTitle"],$pictureType["arTitle"])."'},"; 

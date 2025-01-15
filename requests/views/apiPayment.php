dbedd01b7cf935e7cc8643024da24b7397678cfc
MRxwr <nasserhatab@gmail.com>
2025-01-15 13:10:48 +0300

    Update apiPayment.php

--- requests/views/apiPayment.php
+++ requests/views/apiPayment.php
@@ -80,6 +80,7 @@ curl_setopt_array($curl, array(
 $response = curl_exec($curl);
 $response = json_decode($response, true);
 curl_close($curl);
+echo outputData($response);die();
 if ( $response["status"] == false ) {
     $response = outputError($response);die();
 }else{

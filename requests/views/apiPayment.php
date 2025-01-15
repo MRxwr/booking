e9a16923bb5f86cac5e6cbd7e12812abb5890863
MRxwr <nasserhatab@gmail.com>
2025-01-15 23:11:23 +0300

    Update apiPayment.php

--- requests/views/apiPayment.php
+++ requests/views/apiPayment.php
@@ -56,6 +56,7 @@ if( $vendor[0]["chargeType"] == 1 ){
         }
     }
     if( $extrasCheck = selectDBNew("extras",[$vendor[0]["id"],$_POST["extras"]],"`vendorId` = ? AND `status` = '0' AND `hidden` = '0' AND `id` IN (?)","") ){
+        var_dump($extrasCheck);
         foreach( $extrasCheck as $extra ){
             $price = $price + $extra["price"];
         }

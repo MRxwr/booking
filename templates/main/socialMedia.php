d469c33f88b71ac2f4e07c209f6b4606ec4e4595
MRxwr <nasserhatab@gmail.com>
2025-01-16 12:16:23 +0300

    Update socialMedia.php

--- templates/main/socialMedia.php
+++ templates/main/socialMedia.php
@@ -1,6 +1,6 @@
 <div class="row py-3 m-0">
-	<div class="col-4" style="text-align: -webkit-center;"><?php echo count($serviceList) ?> <div><?php echo direction("Services","خدمة") ?></div></div>
-	<div class="col-4" style="text-align: -webkit-center;"><?php echo count($clientsList) ?> <div><?php echo direction("Clients","عميل") ?></div></div>
+	<div class="col-4" style="text-align: -webkit-center;border-right: 1px solid;"><?php echo count($serviceList) ?> <div><?php echo direction("Services","خدمة") ?></div></div>
+	<div class="col-4" style="text-align: -webkit-center;border-right: 1px solid;"><?php echo count($clientsList) ?> <div><?php echo direction("Clients","عميل") ?></div></div>
 	<div class="col-4" style="text-align: -webkit-center;"><?php echo count($bookingsList) ?> <div><?php echo direction("Bookings","حجز") ?></div></div>
 	<div class="col-md-12 mt-3 d-flex justify-content-center align-items-center">
 		<div class="row p-3 socialMediaBar">

91ce6aec333083a29a9852060562c9d1508e7199
MRxwr <nasserhatab@gmail.com>
2025-01-22 00:28:08 +0300

    Update bladeOrderDetails.php

--- admin/views/bladeOrderDetails.php
+++ admin/views/bladeOrderDetails.php
@@ -63,11 +63,64 @@ if( $order = selectDBNew("bookings",[$_GET["id"]],"`code` = ?","") ){
 ?>
 <!-- Row -->
 <div class="row m-0">
-	<div class="col-sm-12 mb-30">
+	<div class="col-sm-6 mb-30">
 		<div class="panel panel-default card-view">
 			<div class="panel-heading">
 				<div class="text-center">
-					<h6 class="panel-title txt-dark"><?php echo direction("Order Details","تفاصيل الطلب") ?></h6>
+					<h6 class="panel-title txt-dark"><?php echo direction("Client Details","تفاصيل العميل") ?></h6>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="panel-wrapper collapse in">
+				<div class="panel-body ">
+					<form class="mt-30 mb-30" method="POST" action="" enctype="multipart/form-data">
+						<div class="row m-0">
+							<div class="col-md-3">
+								<label><?php echo direction("Date / Time","التاريخ / الوقت") ?></label>
+								<input type="text" name="date" class="form-control" disabled value="<?php echo $order["date"] ?>">
+							</div>
+
+                            <div class="col-md-3">
+                                <label><?php echo direction("Code","الكود") ?></label>
+                                <input type="text" name="code" class="form-control" disabled value="<?php echo $order["code"] ?>">
+                            </div>
+
+                            <div class="col-md-3">
+                                <label><?php echo direction("Vendor","البائع") ?></label>
+                                <input type="text" name="vendor" class="form-control" disabled value="<?php echo $vendor["enTitle"] ?>">
+                            </div>
+
+                            <div class="col-md-3">
+                                <label><?php echo direction("Name","الاسم") ?></label>
+                                <input type="text" name="name" class="form-control" disabled value="<?php echo $customer["name"] ?>">
+                            </div>
+
+                            <div class="col-md-3">
+                                <label><?php echo direction("Email","البريد الالكتروني") ?></label>
+                                <input type="text" name="email" class="form-control" disabled value="<?php echo $customer["email"] ?>">
+                            </div>
+
+                            <div class="col-md-3">
+                                <label><?php echo direction("Phone","الهاتف") ?></label>
+                                <input type="text" name="phone" class="form-control" disabled value="<?php echo $customer["phone"] ?>">
+                            </div>
+							
+							<div class="col-md-12" style="margin-top:10px">
+								<input type="submit" class="btn btn-primary" value="<?php echo direction("Submit","أرسل") ?>">
+								<input type="hidden" name="update" value="0">
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
+    <div class="col-sm-6 mb-30">
+		<div class="panel panel-default card-view">
+			<div class="panel-heading">
+				<div class="text-center">
+					<h6 class="panel-title txt-dark"><?php echo direction("Booking Details","تفاصيل الحجز") ?></h6>
 				</div>
 				<div class="clearfix"></div>
 			</div>
@@ -79,22 +132,7 @@ if( $order = selectDBNew("bookings",[$_GET["id"]],"`code` = ?","") ){
 								<label><?php echo direction("English Title","الإسم الإنجليزي") ?></label>
 								<input type="text" name="enTitle" class="form-control" disabled value="<?php echo $order["enTitle"] ?>">
 							</div>
-
-							<div class="col-md-3">
-								<label><?php echo direction("Arabic Title","الإسم العربي") ?></label>
-								<input type="text" name="arTitle" class="form-control" required>
-							</div>
-
-							<div class="col-md-3">
-								<label><?php echo direction("Icon","الأيقونه") ?></label>
-								<input type="text" name="logo" class="form-control" required>
-							</div>
-
-							<div class="col-md-3">
-								<label><?php echo direction("Link","الرابط") ?></label>
-								<input type="text" name="link" class="form-control" required>
-							</div>
-
+							
 							<div class="col-md-12" style="margin-top:10px">
 								<input type="submit" class="btn btn-primary" value="<?php echo direction("Submit","أرسل") ?>">
 								<input type="hidden" name="update" value="0">
@@ -105,4 +143,5 @@ if( $order = selectDBNew("bookings",[$_GET["id"]],"`code` = ?","") ){
 			</div>
 		</div>
 	</div>
+    
 </div>

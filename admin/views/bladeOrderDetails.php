7b0f8308a62858c5b3a25b6bb3feffcb228a5a22
MRxwr <nasserhatab@gmail.com>
2025-01-22 00:15:46 +0300

    Update bladeOrderDetails.php

--- admin/views/bladeOrderDetails.php
+++ admin/views/bladeOrderDetails.php
@@ -1,9 +1,58 @@
 <?php
-if( $order = selectDBNew("orders2",[$_GET["id"]],"`code` = ?","") ){
+if( $order = selectDBNew("bookings",[$_GET["id"]],"`code` = ?","") ){
     $order = $order[0];
     if( $vendorId != $order["vendorId"] || $vendorId != 0 ){
         header("Location: ?v=Home");die();
     }
+    if ( $vendor = selectDBNew("vendors",[$order["vendorId"]],"`id` = ?","") ){
+        $vendor = $vendor[0];
+        if( $vendor["type"] == 3 ){
+            if( $pictureType = selectDB("picturetype","`id` = '{$order["pictureType"]}'") ){
+                $pictureType = $pictureType[0];
+            }else{
+                $pictureType = array();
+            }
+        }else{
+            $pictureType = array();
+        }
+    }
+    if ( $branch = selectDBNew("branches",[$order["branchId"]],"id = ?","") ){
+        $branch = $branch[0];
+    }
+    if ( $service = selectDBNew("services",[$order["serviceId"]],"id = ?","") ){
+        $service = $service[0];
+    }
+    if( $order["customerDetails"] ){
+        $customer = json_decode($order["customerDetails"],true);
+    }
+    if( $order["extraInfo"] ){
+        $extraInfoOrder = json_decode($order["extraInfo"],true);
+        $keys = array_keys($extraInfoOrder);
+        $extraInfoDetails = array();
+        for( $i = 0; $i < sizeof($keys); $i++ ){
+            $extraInfo = selectDB("extrainfo","`id` = '{$keys[$i]}'");
+            $extraInfoDetails["title"][] = direction($extraInfo["enTitle"],$extraInfo["arTitle"]);
+            $extraInfoDetails["value"][] = $extraInfoOrder[$keys[$i]];
+        }
+    }else{
+        $extraInfoDetails = array();
+    }
+    if( $order["themes"] ){
+        $themes = explode(",",$order["themes"]);
+    }else{
+        $themes = array();
+    }
+    if( $order["extras"] ){
+        $extras = explode(",",$order["extras"]);
+        for( $i = 0; $i < sizeof($extras); $i++ ){
+            $extra = selectDB("extras","`id` = '{$extras[$i]}'");
+            $extrasDetails["title"] = direction($extra["enTitle"],$extra["arTitle"]);
+            $extrasDetails["price"] = $extra["price"];
+        }
+    }else{
+        $extrasDetails = array();
+    }
+
 }
 ?>
 <!-- Row -->
@@ -22,7 +71,7 @@ if( $order = selectDBNew("orders2",[$_GET["id"]],"`code` = ?","") ){
 						<div class="row m-0">
 							<div class="col-md-3">
 								<label><?php echo direction("English Title","الإسم الإنجليزي") ?></label>
-								<input type="text" name="enTitle" class="form-control" required>
+								<input type="text" name="enTitle" class="form-control" disabled value="<?php echo $order["enTitle"] ?>">
 							</div>
 
 							<div class="col-md-3">

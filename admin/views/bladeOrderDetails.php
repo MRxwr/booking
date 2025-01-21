af2f2d1d59a3769f8f03426f288d27533038ed6b
MRxwr <nasserhatab@gmail.com>
2025-01-22 01:15:11 +0300

    Update bladeOrderDetails.php

--- admin/views/bladeOrderDetails.php
+++ admin/views/bladeOrderDetails.php
@@ -31,7 +31,7 @@ if( $order = selectDBNew("bookings",[$_GET["id"]],"`code` = ?","") ){
     if( $order["customerDetails"] ){
         $customer = json_decode($order["customerDetails"],true);
     }
-    if( $order["extraInfo"] != "null" || $order["extraInfo"] != "" ){
+    if( !empty($order["extraInfo"]) || $order["extraInfo"] != "null" ){
         $extraInfoOrder = json_decode($order["extraInfo"],true);
         $keys = array_keys($extraInfoOrder);
         $extraInfoDetails = array();

d60a3d3612a871a4f54414ab8511155fa57833ee
MRxwr <nasserhatab@gmail.com>
2025-01-24 01:23:12 +0300

    Update powered.php

--- templates/theme-2/powered.php
+++ templates/theme-2/powered.php
@@ -1,6 +1,6 @@
 <hr>
 <div class="row m-0 w-100 mt-3 mb-3">
-	<div class="col-12 p-0 d-block d-sm-none">
+	<div class="col-12 p-0 d-block">
 		<div class="row m-0 w-100">
 			<div class="col-12 text-center">
 				<img src="https://i.imgur.com/BW29zDw.jpg" class="poweredMobile" >

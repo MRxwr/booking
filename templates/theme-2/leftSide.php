1926db8197bd352b3bf58693e4414400b986924f
MRxwr <nasserhatab@gmail.com>
2025-01-23 23:36:17 +0300

    Update leftSide.php

--- templates/theme-2/leftSide.php
+++ templates/theme-2/leftSide.php
@@ -1,35 +1,35 @@
 <div class="col-12" id="leftSide">
 	<!-- header -->
-	<?php require_once("templates/main/header.php") ?>
+	<?php require_once("templates/{$theme}/header.php") ?>
 	<!-- end of header -->
 
 	<!-- mobile hero section -->
-	<?php require_once("templates/main/hero.php") ?>
+	<?php require_once("templates/{$theme}/hero.php") ?>
 	<!-- end of mobile hero section -->
 
 	<!-- social Media Bar -->
-	<?php require_once("templates/main/socialMedia.php") ?>
+	<?php require_once("templates/{$theme}/socialMedia.php") ?>
 	<!-- end of social media bar -->
 	 
 	<!-- rest of page -->
 	<?php
 	if( isset($_GET["status"]) && $_GET["status"] == "success" ){
 		//cal success
-		require_once("templates/main/success.php");
+		require_once("templates/{$theme}/success.php");
 	}elseif( isset($_GET["status"]) && $_GET["status"] == "failure" ){
 		//call failure
-		require_once("templates/main/failure.php");
+		require_once("templates/{$theme}/failure.php");
 	}else{
 		//call selections
-		require_once("templates/main/selections.php");
+		require_once("templates/{$theme}/selections.php");
 		
 		//call info
-		require_once("templates/main/info.php");
+		require_once("templates/{$theme}/info.php");
 	}
 	?>
 	<!-- end of rest of page -->
 	
 	<!-- mobile footer -->
-	<?php require_once("templates/main/powered.php") ?>
+	<?php require_once("templates/{$theme}/powered.php") ?>
 	<!-- end of mobile footer -->
 </div>

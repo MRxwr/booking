ee65a73100c133e2f6a99a777f083ee6590e67a5
MRxwr <nasserhatab@gmail.com>
2025-01-24 00:10:21 +0300

    Update hero.php

--- templates/theme-2/hero.php
+++ templates/theme-2/hero.php
@@ -1,5 +1,5 @@
 <div class="row m-0 w-100">
-	<div class="col-12 p-0 pb-5 d-block d-md-none">
+	<div class="col-12 p-0 pb-5 d-block">
 		<div class="heroBg">
 			<div class="heroLogoBg" style="align-content: center;border: 1px solid;box-shadow: 0px 0px 10px 0px #757575;<?php echo direction("top: 125px;left: 35%;","top: 125px;left: -35%;") ?>">
 				<img src="<?php echo "logos/{$vendor["logo"]}" ?>" class="heroLogo">

bb3388807a56d398cdda527b2d3be0911f2ee8f4
MRxwr <nasserhatab@gmail.com>
2025-01-24 00:01:33 +0300

    save

--- templates/main/leftSide.php
+++ templates/main/leftSide.php
@@ -1,35 +1,40 @@
-<div class="col-5 p-0" id="leftSide">
-	<!-- header -->
-	<?php require_once("templates/main/header.php") ?>
-	<!-- end of header -->
+<div class="container-fluid p-0">
+	<div class="row w-100 m-0">
+	<div id="loading-screen" style="display: block;">
+		<img src="img/loading.png" alt="Loading...">
+	</div>
+	<div class="col-5 p-0" id="leftSide">
+		<!-- header -->
+		<?php require_once("templates/main/header.php") ?>
+		<!-- end of header -->
 
-	<!-- mobile hero section -->
-	<?php require_once("templates/main/hero.php") ?>
-	<!-- end of mobile hero section -->
+		<!-- mobile hero section -->
+		<?php require_once("templates/main/hero.php") ?>
+		<!-- end of mobile hero section -->
 
-	<!-- social Media Bar -->
-	<?php require_once("templates/main/socialMedia.php") ?>
-	<!-- end of social media bar -->
-	 
-	<!-- rest of page -->
-	<?php
-	if( isset($_GET["status"]) && $_GET["status"] == "success" ){
-		//cal success
-		require_once("templates/main/success.php");
-	}elseif( isset($_GET["status"]) && $_GET["status"] == "failure" ){
-		//call failure
-		require_once("templates/main/failure.php");
-	}else{
-		//call selections
-		require_once("templates/main/selections.php");
+		<!-- social Media Bar -->
+		<?php require_once("templates/main/socialMedia.php") ?>
+		<!-- end of social media bar -->
 		
-		//call info
-		require_once("templates/main/info.php");
-	}
-	?>
-	<!-- end of rest of page -->
-	
-	<!-- mobile footer -->
-	<?php require_once("templates/main/powered.php") ?>
-	<!-- end of mobile footer -->
-</div>
+		<!-- rest of page -->
+		<?php
+		if( isset($_GET["status"]) && $_GET["status"] == "success" ){
+			//cal success
+			require_once("templates/main/success.php");
+		}elseif( isset($_GET["status"]) && $_GET["status"] == "failure" ){
+			//call failure
+			require_once("templates/main/failure.php");
+		}else{
+			//call selections
+			require_once("templates/main/selections.php");
+			
+			//call info
+			require_once("templates/main/info.php");
+		}
+		?>
+		<!-- end of rest of page -->
+		
+		<!-- mobile footer -->
+		<?php require_once("templates/main/powered.php") ?>
+		<!-- end of mobile footer -->
+	</div>
--- templates/theme-2/leftSide.php
+++ templates/theme-2/leftSide.php
@@ -1,35 +1,40 @@
-<div class="col-12" id="leftSide">
-	<!-- header -->
-	<?php require_once("templates/{$vendorTheme}/header.php") ?>
-	<!-- end of header -->
+<div class="container-fluid p-0">
+	<div class="row w-100 m-0">
+	<div id="loading-screen" style="display: block;">
+		<img src="img/loading.png" alt="Loading...">
+	</div>
+	<div class="col-12 p-0" id="leftSide">
+		<!-- header -->
+		<?php require_once("templates/{$vendorTheme}/header.php") ?>
+		<!-- end of header -->
 
-	<!-- mobile hero section -->
-	<?php require_once("templates/{$vendorTheme}/hero.php") ?>
-	<!-- end of mobile hero section -->
+		<!-- mobile hero section -->
+		<?php require_once("templates/{$vendorTheme}/hero.php") ?>
+		<!-- end of mobile hero section -->
 
-	<!-- social Media Bar -->
-	<?php require_once("templates/{$vendorTheme}/socialMedia.php") ?>
-	<!-- end of social media bar -->
-	 
-	<!-- rest of page -->
-	<?php
-	if( isset($_GET["status"]) && $_GET["status"] == "success" ){
-		//cal success
-		require_once("templates/{$vendorTheme}/success.php");
-	}elseif( isset($_GET["status"]) && $_GET["status"] == "failure" ){
-		//call failure
-		require_once("templates/{$vendorTheme}/failure.php");
-	}else{
-		//call selections
-		require_once("templates/{$vendorTheme}/selections.php");
+		<!-- social Media Bar -->
+		<?php require_once("templates/{$vendorTheme}/socialMedia.php") ?>
+		<!-- end of social media bar -->
+		
+		<!-- rest of page -->
+		<?php
+		if( isset($_GET["status"]) && $_GET["status"] == "success" ){
+			//cal success
+			require_once("templates/{$vendorTheme}/success.php");
+		}elseif( isset($_GET["status"]) && $_GET["status"] == "failure" ){
+			//call failure
+			require_once("templates/{$vendorTheme}/failure.php");
+		}else{
+			//call selections
+			require_once("templates/{$vendorTheme}/selections.php");
 
-		//call info
-		require_once("templates/{$vendorTheme}/info.php");
-	}
-	?>
-	<!-- end of rest of page -->
-	
-	<!-- mobile footer -->
-	<?php require_once("templates/{$vendorTheme}/powered.php") ?>
-	<!-- end of mobile footer -->
-</div>
+			//call info
+			require_once("templates/{$vendorTheme}/info.php");
+		}
+		?>
+		<!-- end of rest of page -->
+		
+		<!-- mobile footer -->
+		<?php require_once("templates/{$vendorTheme}/powered.php") ?>
+		<!-- end of mobile footer -->
+	</div>

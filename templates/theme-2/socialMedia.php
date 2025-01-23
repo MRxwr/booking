19a8eca16cee2fae32a88bde7cff1ae73a5a3ed9
MRxwr <nasserhatab@gmail.com>
2025-01-24 01:16:29 +0300

    Update socialMedia.php

--- templates/theme-2/socialMedia.php
+++ templates/theme-2/socialMedia.php
@@ -1,9 +1,6 @@
 <div class="row py-3 m-0">
 	<div class="col-sm-2 col-3" style="text-align: -webkit-center;"><img src="<?php echo "logos/{$vendor["logo"]}" ?>" class="heroLogo"></div>
 	<div class="col-sm-10 col-9" style="font-size: 12px;align-content: center;"><?php echo direction($vendor["enDetails"],$vendor["arDetails"]) ?></div>
-	<div class="col-4" style="text-align: -webkit-center;"><?php echo count($serviceList) ?> <div><?php echo direction("Services","خدمة") ?></div></div>
-	<div class="col-4" style="text-align: -webkit-center;border-right: 1px solid; border-left: 1px solid"><?php echo count($clientsList) ?> <div><?php echo direction("Clients","عميل") ?></div></div>
-	<div class="col-4 mb-3" style="text-align: -webkit-center;"><?php echo count($bookingsList) ?> <div><?php echo direction("Bookings","حجز") ?></div></div>
 		<?php
 		if( !empty($vendor["socialMedia"]) ){
 			$socialMedia = json_decode($vendor["socialMedia"],true);
@@ -24,4 +21,7 @@
 			}
 		}
 		?>
+		<div class="col-4" style="text-align: -webkit-center;"><?php echo count($serviceList) ?> <div><?php echo direction("Services","خدمة") ?></div></div>
+		<div class="col-4" style="text-align: -webkit-center;border-right: 1px solid; border-left: 1px solid"><?php echo count($clientsList) ?> <div><?php echo direction("Clients","عميل") ?></div></div>
+		<div class="col-4 mb-3" style="text-align: -webkit-center;"><?php echo count($bookingsList) ?> <div><?php echo direction("Bookings","حجز") ?></div></div>
 </div>

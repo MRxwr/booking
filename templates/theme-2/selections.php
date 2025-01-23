0139704adb2f7114fc4c31d5066dbc06190323e3
MRxwr <nasserhatab@gmail.com>
2025-01-24 01:00:09 +0300

    Update selections.php

--- templates/theme-2/selections.php
+++ templates/theme-2/selections.php
@@ -428,7 +428,7 @@ if( $vendor["type"] == "3" ){
 	}
 	$.each(filteredThemes, function(key, value) {
 		var image = value.image;
-		var themes = "<div class='col-sm-2 col-4 pb-1'><img src='logos/"+image+"' class='themeInput' id='"+image+"' style='width:100px; height:100px'></a></div>";
+		var themes = "<div class='col-sm-2 col-4 pb-1'><img src='logos/"+image+"' class='themeInput' id='"+image+"' style='width:100%; height:120px'></a></div>";
 		$(".themesBLK").append(themes);
 	});
 	});

992444d2a5d996179dafbb89d9dab60fbaea93c4
MRxwr <nasserhatab@gmail.com>
2025-04-09 22:44:42 +0300

    Update default.php

--- default.php
+++ default.php
@@ -363,52 +363,9 @@
         <i class="fas fa-arrow-up"></i>
     </div>
 
+    <script src="js/script.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script>
-        $(document).ready(function () {
-            // Navbar scroll effect
-            $(window).scroll(function () {
-                if ($(this).scrollTop() > 100) {
-                    $('.navbar').addClass('scrolled');
-                    $('#backToTop').addClass('active');
-                } else {
-                    $('.navbar').removeClass('scrolled');
-                    $('#backToTop').removeClass('active');
-                }
-            });
-            
-            // Smooth scrolling for nav links
-            $('.nav-link, .footer-links a').on('click', function (e) {
-                if (this.hash !== '') {
-                    e.preventDefault();
-                    const hash = this.hash;
-                    $('html, body').animate({
-                        scrollTop: $(hash).offset().top - 100
-                    }, 800);
-                }
-            });
-            
-            // Back to top button
-            $('#backToTop').on('click', function () {
-                $('html, body').animate({ scrollTop: 0 }, 800);
-            });
-            
-            // Animation on scroll
-            $(window).scroll(function() {
-                $('.fade-in').each(function() {
-                    const elementTop = $(this).offset().top;
-                    const elementBottom = elementTop + $(this).outerHeight();
-                    const viewportTop = $(window).scrollTop();
-                    const viewportBottom = viewportTop + $(window).height();
-                    
-                    if (elementBottom > viewportTop && elementTop < viewportBottom) {
-                        $(this).addClass('visible');
-                    }
-                });
-            });
-        });
-    </script>
 </body>
 </html>

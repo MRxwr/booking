7531a9475aced2668e06d6eb701a52ff4018cf11
MRxwr <nasserhatab@gmail.com>
2025-04-19 01:14:50 +0300

    save

--- requests/dashboard/views/apiSystems.php
+++ requests/dashboard/views/apiSystems.php
@@ -19,8 +19,48 @@ if( isset($_GET["action"]) && !empty($_GET["action"]) ){
             $response = array("msg" => checkAPILanguege("Invalid token.", "التوكن غير صالح."));
             echo outputError($response);die();
         }
-    }
-                
+    }elseif( $action == "add" ){
+        if ( !$token ){
+            $response = array("msg" => checkAPILanguege("Token is required.", "التوكن مطلوب."));
+            echo outputError($response);die();
+        }
+        if( !isset($data["enTitle"]) || empty($data["enTitle"]) ){
+            $response = array("msg" => checkAPILanguege("English title is required.", "العنوان باللغة الإنجليزية مطلوب."));
+            echo outputError($response);die();
+        }
+        if( !isset($data["arTitle"]) || empty($data["arTitle"]) ){
+            $response = array("msg" => checkAPILanguege("Arabic title is required.", "العنوان باللغة العربية مطلوب."));
+            echo outputError($response);die();
+        }
+        if( !isset($_FILES["logo"]) || empty($_FILES["logo"]) ){
+            $response = array("msg" => checkAPILanguege("Logo is required.", "الشعار مطلوب."));
+            echo outputError($response);die();
+        }
+        if( !isset($_FILES["coverImg"]) || empty($_FILES["coverImg"]) ){
+            $response = array("msg" => checkAPILanguege("Cover image is required.", "صورة الغلاف مطلوبة."));
+            echo outputError($response);die();
+        }
+        if( !isset($data["type"]) || empty($data["type"]) ){
+            $response = array("msg" => checkAPILanguege("Type is required.", "النوع مطلوب."));
+            echo outputError($response);die();
+        }
+        if( !isset($data["url"]) || empty($data["url"]) ){
+            $response = array("msg" => checkAPILanguege("URL is required.", "الرابط مطلوب."));
+            echo outputError($response);die();
+        }elseif( $url = selectDBNew("vendors",[$data["url"]],"`url` LIKE ? AND `status` = 0","") ){
+            $response = array("msg" => checkAPILanguege("URL already exists. Please choose another one.", "الرابط موجود مسبقاً. يرجى اختيار رابط اخر."));
+            echo outputError($response);die();
+        }
+        $data["logo"] = uploadImageAPI($_FILES["logo"]["tmp_name"]);
+        $date["coverImg"] = uploadImageAPI($_FILES["coverImg"]["tmp_name"]);
+        if( insertDB("vendors",$data) ){
+            $response = array("msg" => checkAPILanguege("Booking System has been added successfully.", "تم إضافة نظام الحجز بنجاح."));
+            echo outputData($response);die();
+        }else{
+            $response = array("msg" => checkAPILanguege("Failed to add booking system.", "فشل إضافة نظام الحجز."));
+            echo outputError($response);die();
+        }
+    }       
 }else{
     $error = array("msg"=>"Wrong Action Request 404");
     echo outputError($error);die();

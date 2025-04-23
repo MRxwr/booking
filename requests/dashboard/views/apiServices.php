696c6dbc077c58697df73166ed8192798a25b4d2
MRxwr <nasserhatab@gmail.com>
2025-04-24 01:08:37 +0300

    Update apiServices.php

--- requests/dashboard/views/apiServices.php
+++ requests/dashboard/views/apiServices.php
@@ -135,10 +135,11 @@ if( isset($_GET["action"]) && !empty($_GET["action"]) ){
         }
         if( $service = selectDBNew("services",[$data["id"]],"`id` = ?","") ){
             $listTypes = json_decode($service[0]["listTypes"],true);
-            var_dump($listTypes);die();
+            
             if( !is_null($listTypes) && isset($listTypes[$data["index"]]) ){
                 unset($listTypes[$data["index"]]);
                 $listTypes = array_values($listTypes);
+                var_dump($listTypes);die();
                 $data["listTypes"] = json_encode($listTypes);
                 if( updateDB("services", $data, "`id` = {$data["id"]}") ){
                     $response = array("msg" => checkAPILanguege("List Types Deleted Successfully", "تم حذف قائمة الأنواع بنجاح"));

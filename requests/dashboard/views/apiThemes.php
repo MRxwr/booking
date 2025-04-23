00f944d46bf2645ea3e8bc3984a13ceb65ff52cc
MRxwr <nasserhatab@gmail.com>
2025-04-23 23:17:46 +0300

    Create apiThemes.php

---
+++ requests/dashboard/views/apiThemes.php
@@ -0,0 +1,84 @@
+<?php
+if( isset($_GET["action"]) && !empty($_GET["action"]) ){
+    $action = $_GET["action"];
+    $data = (isset($_POST) && !empty($_POST)) ? $_POST : array();
+    if ( !$token ){
+        $response = array("msg" => checkAPILanguege("Token is required.", "التوكن مطلوب."));
+        echo outputError($response);die();
+    }elseif( $user = selectDBNew("employees",[$token],"`keepMeAlive` LIKE ? AND `status` = 0","") ){
+        $data["vendorId"] = $user[0]["id"];
+    }
+    if( $action == "list" ){
+        if( $Themes = selectDB2New("`id`, `enTitle`, `arTitle`, `themes`, `hidden`","themes",[$data["vendorId"]],"`status` = 0 AND `vendorId` = ?","") ){
+            echo outputData($Themes);die();
+        }else{
+            $response = array("msg" => checkAPILanguege("No Themes Found", "لا توجد التصاميم متاحة"));
+            echo outputError($response);die();
+        }
+    }elseif( $action == "add" ){
+        if( !isset($data["enTitle"]) || empty($data["enTitle"]) ){
+            $response = array("msg" => checkAPILanguege("English Title is required.", "العنوان باللغة الإنجليزية مطلوب."));
+            echo outputError($response);die();
+        }
+        if( !isset($data["arTitle"]) || empty($data["arTitle"]) ){
+            $response = array("msg" => checkAPILanguege("Arabic Title is required.", "العنوان باللغة العربية مطلوب."));
+            echo outputError($response);die();
+        }
+        if( !isset($data["hidden"]) ){
+            $response = array("msg" => checkAPILanguege("Hidden is required.", "المخفي مطلوب."));
+            echo outputError($response);die();
+        }
+        if( insertDB("themes", $data) ){
+            $response = array("msg" => checkAPILanguege("Themes Added Successfully", "تمت إضافة التصميم بنجاح"));
+            echo outputData($response);die();
+        }else{
+            $response = array("msg" => checkAPILanguege("Failed to Add Themes", "فشل في إضافة التصميم"));
+            echo outputError($response);die();
+        }
+    }elseif( $action == "update"){
+        if( !isset($data["id"]) || empty($data["id"]) ){
+            $response = array("msg" => checkAPILanguege("ID is required.", "المعرف مطلوب."));
+            echo outputError($response);die();
+        }
+        if( !isset($data["enTitle"]) || empty($data["enTitle"]) ){
+            $response = array("msg" => checkAPILanguege("English Title is required.", "العنوان باللغة الإنجليزية مطلوب."));
+            echo outputError($response);die();
+        }
+        if( !isset($data["arTitle"]) || empty($data["arTitle"]) ){
+            $response = array("msg" => checkAPILanguege("Arabic Title is required.", "العنوان باللغة العربية مطلوب."));
+            echo outputError($response);die();
+        }
+        if( !isset($data["hidden"]) ){
+            $response = array("msg" => checkAPILanguege("Hidden is required.", "المخفي مطلوب."));
+            echo outputError($response);die();
+        }
+        if( updateDB("themes", $data, "`id` = {$data["id"]}") ){
+            $response = array("msg" => checkAPILanguege("Themes Updated Successfully", "تم تحديث التصميم بنجاح"));
+            echo outputData($response);die();
+        }else{
+            $response = array("msg" => checkAPILanguege("Failed to Update Themes", "فشل في تحديث التصميم"));
+            echo outputError($response);die();
+        }
+    }elseif( $action == "delete" ){
+        if( !isset($data["id"]) || empty($data["id"]) ){
+            $response = array("msg" => checkAPILanguege("ID is required.", "المعرف مطلوب."));
+            echo outputError($response);die();
+        }
+        if( updateDB("themes", array("status" => 1),"`id` = {$data["id"]}") ){
+            $response = array("msg" => checkAPILanguege("Themes Deleted Successfully", "تم حذف التصميم بنجاح"));
+            echo outputData($response);die();
+        }else{
+            $response = array("msg" => checkAPILanguege("Failed to Delete Themes", "فشل حذف التصميم"));
+            echo outputError($response);die();
+        }
+    }elseif( $action == "addTheme" ){
+
+    }elseif( $action == "daleteTheme" ){
+
+    }else{
+        $response = array("msg" => checkAPILanguege("Wrong Endpoint Request 404", "خطأ في طلب نقطة النهاية 404"));
+        echo outputError($response);die();
+    }
+} 
+
+?>

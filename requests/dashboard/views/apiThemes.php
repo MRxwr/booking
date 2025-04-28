de7e0d625bc7c32b14f17d281fdf8b546f898d96
MRxwr <nasserhatab@gmail.com>
2025-04-28 21:42:41 +0300

    Update apiThemes.php

--- requests/dashboard/views/apiThemes.php
+++ requests/dashboard/views/apiThemes.php
@@ -11,8 +11,22 @@ if( isset($_GET["action"]) && !empty($_GET["action"]) ){
     if( $action == "list" ){
         if( $Themes = selectDB2New("`id`, `enTitle`, `arTitle`, `themes`, `hidden`","themes",[$data["vendorId"]],"`status` = 0 AND `vendorId` = ?","") ){
             for( $i = 0 ; $i < sizeof($Themes); $i++ ){
-                if( !is_null($Themes[$i]["themes"]) ){
-                    $Themes[$i]["themes"] = json_decode($Themes[$i]["themes"],true);
+                if( !is_null($Themes[$i]["themes"]) && $Themes[$i]["themes"] !== '' ){
+                    $Themes[$i]["themes"] = json_decode($Themes[$i]["themes"], true);
+                    // Ensure themes is always a simple array, not nested objects
+                    if(is_array($Themes[$i]["themes"]) && !empty($Themes[$i]["themes"])) {
+                        // Check if the first element is an associative array (object)
+                        if(isset($Themes[$i]["themes"][0]) && is_array($Themes[$i]["themes"][0]) && isset($Themes[$i]["themes"][0]["id"])) {
+                            // Extract only image paths
+                            $cleanedThemes = [];
+                            foreach($Themes[$i]["themes"] as $theme) {
+                                if(is_string($theme)) {
+                                    $cleanedThemes[] = $theme;
+                                }
+                            }
+                            $Themes[$i]["themes"] = $cleanedThemes;
+                        }
+                    }
                 }
             }
             echo outputData($Themes);die();

b656366219ede8ae51bb2ad6e31ee3d751bb38f1
MRxwr <nasserhatab@gmail.com>
2025-05-07 01:19:19 +0300

    save

--- admin/includes/functions/payment.php
+++ admin/includes/functions/payment.php
@@ -55,4 +55,44 @@ function checkPayment($data){
 	];
 	return $array;
 }
+
+function upaymentSubscription($employee, $package){
+	$settings = selectDB("settings","`id` = '1'");
+	$orderId = date("Ymd").rand(00,99).time();
+	$postBody = array(
+		'language' => 'en',
+		'order[id]' => $orderId,
+		'order[currency]' => 'KWD',
+		'order[amount]' => (string)$package[0]["price"],
+		'order[description]' => "order for {$employee[0]["name"]}, {$employee[0]["phone"]}",
+		'reference[id]' => $orderId,
+		'customer[name]' => "{$employee[0]["name"]}",
+		'customer[email]' => "{$employee[0]["email"]}",
+		'customer[mobile]' => "{$employee[0]["phone"]}",
+		'returnUrl' => 'https://reservaa.com/index.php',
+		'cancelUrl' => 'https://reservaa.com/index.php',
+		'notificationUrl' => 'https://reservaa.com/index.php',
+		);
+
+	$curl = curl_init();
+	curl_setopt_array($curl, array(
+		CURLOPT_URL => 'https://uapi.upayments.com/api/v1/charge',
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => '',
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 0,
+		CURLOPT_FOLLOWLOCATION => true,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => 'POST',
+		CURLOPT_POSTFIELDS => $postBody,
+		CURLOPT_HTTPHEADER => array(
+			"Authorization: {$settings[0]["upaymentToken"]}",
+		),
+	));
+	$response = curl_exec($curl);
+	curl_close($curl);
+	$response = json_decode($response,true);
+	$response["orderId"] = $orderId;
+	return $response;
+}
 ?>
---
+++ requests/dashboard/views/apiPayment.php
@@ -0,0 +1,49 @@
+<?php
+if( isset($_GET["action"]) && !empty($_GET["action"]) ){
+    $action = $_GET["action"];
+    $data = (isset($_POST) && !empty($_POST)) ? $_POST : array();
+    if ( !$token ){
+        $response = array("msg" => checkAPILanguege("Token is required.", "التوكن مطلوب."));
+        echo outputError($response);die();
+    }elseif( $user = selectDBNew("employees",[$token],"`keepMeAlive` LIKE ? AND `status` = 0","") ){
+        $data["vendorId"] = $user[0]["id"];
+    }
+    if( $action == "submit" ){
+        if( isset($data["packageId"]) && !empty($data["packageId"]) && $package = selectDB("packages","`id` = '{$data["packageId"]}' AND `status` = 0 AND `hidden` = 0") ){
+            if( $response = upaymentSubscription($user,$package) ){
+                if( isset($response["status"]) && $response["status"] == true && isset($response["data"]["link"]) && !empty($response["data"]["link"]) ){
+                    $data["gatewayId"] = $response["orderId"];
+                    if( insertDB("subscriptions",$data) ){
+                        echo outputData(array("url" => $response["data"]["link"],"orderId" => $response["orderId"]));die();
+                    }else{
+                        echo outputError(array("msg" => checkAPILanguege($lang,"Could not create order","لا يمكن انشاء الطلب") ));die();
+                    }
+                }else{
+                    echo outputError(array("msg" => checkAPILanguege($lang,"Payment gateway error","خطأ في بوابة الدفع") ));die();
+                }
+            }else{
+                echo outputError(array("msg" => checkAPILanguege($lang,"Something went wrong","حدث خطأ ما") ));die();
+            }
+        }else{
+            $response = array("msg" => checkAPILanguege("Package not found", "الباقة غير موجودة"));
+            echo outputError($response);die();
+        }
+            if( insertDB("subscriptions",$data) ){
+                echo outputData($data);die();
+            }else{
+                $response = array("msg" => checkAPILanguege("Error in inserting data", "خطأ في إدخال البيانات"));
+                echo outputError($response);die();
+            }
+        if( $Packages = selectDB("packages","`status` = 0 AND `hidden` = 0") ){
+            echo outputData($Packages);die();
+        }else{
+            $response = array("msg" => checkAPILanguege("No packages Found", "لا توجد باقات  متاحة"));
+            echo outputError($response);die();
+        }
+    }else{
+        $response = array("msg" => checkAPILanguege("Wrong Endpoint Request 404", "خطأ في طلب نقطة النهاية 404"));
+        echo outputError($response);die();
+    }
+} 
+
+?>

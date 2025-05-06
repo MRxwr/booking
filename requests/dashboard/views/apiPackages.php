45d7165562e3723f8b0a225c2c290c4d6ac777fe
MRxwr <nasserhatab@gmail.com>
2025-05-06 18:48:43 +0300

    Create apiPackages.php

---
+++ requests/dashboard/views/apiPackages.php
@@ -0,0 +1,24 @@
+<?php
+if( isset($_GET["action"]) && !empty($_GET["action"]) ){
+    $action = $_GET["action"];
+    $data = (isset($_POST) && !empty($_POST)) ? $_POST : array();
+    if ( !$token ){
+        $response = array("msg" => checkAPILanguege("Token is required.", "التوكن مطلوب."));
+        echo outputError($response);die();
+    }elseif( $user = selectDBNew("employees",[$token],"`keepMeAlive` LIKE ? AND `status` = 0","") ){
+        $data["vendorId"] = $user[0]["id"];
+    }
+    if( $action == "list" ){
+        if( $BlockedDays = selectDB2New("*","packages",[$data["vendorId"]],"`status` = 0 AND `vendorId` = ?","") ){
+            echo outputData($BlockedDays);die();
+        }else{
+            $response = array("msg" => checkAPILanguege("No packages Found", "لا توجد باقات  متاحة"));
+            echo outputError($response);die();
+        }
+    }else{
+        $response = array("msg" => checkAPILanguege("Wrong Endpoint Request 404", "خطأ في طلب نقطة النهاية 404"));
+        echo outputError($response);die();
+    }
+} 
+
+?>
